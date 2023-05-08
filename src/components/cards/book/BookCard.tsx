54571a68c1f271a69bd55c1f7b019bf898ac87a5
Steshenko Dmitry <3301297@gmail.com>
2023-05-08 05:21:12 +0300

    feat: add book card styles

--- src/components/cards/book/BookCard.tsx
+++ src/components/cards/book/BookCard.tsx
@@ -9,10 +9,12 @@ interface BookCardProps {
 const BookCard = (props: BookCardProps) => {
   return (
     <div className={styles.bookCard}>
-      <img src={props.book.image} alt="book cover" />
+      <div className={styles.image}>
+        <img src={props.book.image} alt="book cover" />
+      </div>
       <span className={styles.name}>{props.book.name}</span>
       <span className={styles.author}>
-        `${props.book.author.name} ${props.book.author.surname}`
+        {`${props.book.author.name} ${props.book.author.surname}`}
       </span>
     </div>
   );
